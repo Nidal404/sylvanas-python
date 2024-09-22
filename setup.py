8e77f84fe09e12d2d62fd8c8ac772e1c9ac45836
Nidal <dwalibi.nidal@gmail.com>
2024-09-22 13:26:07 +0200

    dev

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="sylvanas",
-    version="0.1.0",
+    version='0.0.1',
+    author='N.i.d.a.l',
+    description='Coucou',
     packages=find_packages(),
     install_requires=[
         "cryptography == 43.0.1"
@@ -11,11 +13,5 @@ setup(
         "pytest == 8.3.3"
         "SQLAlchemy == 2.0.35"
         "SQLAlchemy - Utils == 0.41.2"
-    ],
-    author="Nidal",
-    long_description=open("README.md").read(),
-    python_requires=">=3.8",
-    url="https://github.com/Nidal404/sylvanas-python",
-    classifiers=[
-    ],
+    ]
 )
