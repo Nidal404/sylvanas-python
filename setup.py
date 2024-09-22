34a10302f67ed057bb97b568bd5ba2449375a8a0
Nidal <dwalibi.nidal@gmail.com>
2024-09-22 13:23:49 +0200

    dev

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "PyMySQL==1.1.1"
+        "cryptography == 43.0.1"
+        "jsonschema == 4.23.0"
+        "PyMySQL == 1.1.1"
+        "pytest == 8.3.3"
+        "SQLAlchemy == 2.0.35"
+        "SQLAlchemy - Utils == 0.41.2"
     ],
     author="Nidal",
     long_description=open("README.md").read(),
