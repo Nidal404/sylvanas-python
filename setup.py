51eb922db69f24e68b97d33762f716c8b0528879
Nidal <dwalibi.nidal@gmail.com>
2024-09-22 13:20:16 +0200

    dev

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        # Ajoutez ici les dépendances de votre projet
+        "PyMySQL==1.1.1"
     ],
     author="Nidal",
     long_description=open("README.md").read(),
     python_requires=">=3.8",
+    url="https://github.com/Nidal404/sylvanas-python",
+    classifiers=[
+    ],
 )
